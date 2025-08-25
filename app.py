04f3c5b17de3bafaa06557a7cc9fee31b1fb9e90
joaoSandovall <jpsandovalveras@gmail.com>
2025-08-25 13:30:15 -0300

    second commit

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-def adicionar_contato(nome, telefone, email):
-    contatos_exemplo = {
-        "nome": "joao",
-        "telefone": "6191895543",
-        "e-mail": "joao@emailcom",
-        "favorito": False
-        }
-    contatos.append(contatos_exemplo)
-    print(f"contato {nome} foi adicionado")
-    return
+# funções
+def adicionar_contato(lista_de_contatos):
+    nome = input("Digite o nome do contato: ")
+    telefone = input("Digite o telefone: ")
+    email = input("Digite o email: ")
     
+    novo_contato = {
+        "nome": nome,
+        "telefone": telefone,
+        "email": email,
+        "favorito": False
+    }
+    
+    lista_de_contatos.append(novo_contato)
+    
+    print(f"O contato {nome} foi adicionado com sucesso.")
+    pass
+
+def visualizar_contato(lista_de_contatos):
+    print("\n Lista de contatos")
+    
+    if not lista_de_contatos:
+        print("Sua lista de contatos está vazia.")
+        return
+    for indice, contato in enumerate(lista_de_contatos):
+        status_favorito = "★" if contato["favorito"] else " "
+        nome_contato = contato["nome"]
+        telefone_contato = contato["telefone"]
+        email_contato = contato["email"]
+        
+        print(f"{indice + 1}. [{status_favorito}] Nome: {nome_contato} - Tel: {telefone_contato} - Email: {email_contato}")
+print("---------------------")
+    
+
+
 contatos = []
 while True:
     print("\ncadastro e visualizalçao de contatos")
@@ -25,8 +49,15 @@ while True:
 # escolhas
 
     if escolha == "1":
-        adicionar_contato = input("Digite o nome da pessoa")
+        adicionar_contato(contatos)
+        
+    elif escolha == "2":
+        visualizar_contato(contatos)
     
-    break
-
+    elif escolha == "3":
+        editar_contato(contatos)
+    
+    elif escolha == "7":
+        break
+        
 print("programa finalizado")
